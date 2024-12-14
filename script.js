57c4389b9e1c8534b215ddc183047e25ad5547a6
unknown <kimjinyeon16@gmail.com>
2024-12-14 11:15:26 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById("form_registro").addEventListener("submit", function(event){
+    event.preventDefault();
+    const nombres = document.getElementById("nombres_apellidos").value;
+    const edad = document.getElementById("edad").value;
+    const nacionalidad = document.getElementById("nacionalidad").value;
+    const genero = document.getElementById("genero").value;
+    const numero= document.getElementById("numero").value;
+    const correo = document.getElementById("correo").value;
+    const direccion = document.getElementById("direccion").value;
+    
+    if(edad < 18){
+    
+        alert("Debe ser mayor de 18 aÃ±os");
+        return;
+    }
+    
+    if(nacionalidad!=="Peruano"){
+        alert("Este evento es realizado solo para peruanos");
+        return;
+    }
+    if(edad>100){
+        alert("Ingresar una edad valida");
+        return;
+    }
+
+    const tablebody2xd = document.createElement('tr');
+    const tablebody = document.getElementById("tablebody");
+    tablebody2xd.innerHTML=`
+        <tr>
+            <td>${nombres}</td>
+            <td>${edad}</td>
+            <td>${nacionalidad}</td>
+            <td>${genero}</td>
+            <td>${numero}</td>
+            <td>${correo}</td>
+            <td>${direccion}</td>
+        <tr>
+    `;
+
+    tablebody.appendChild(tablebody2xd);
+    document.getElementById("tablas_datos").style.display="";
+
+});
